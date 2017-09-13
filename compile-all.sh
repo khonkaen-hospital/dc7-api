cd8a9a619357be053066696cbdd414285bb754d7
Satit Seethaphon <dixonsatit@gmail.com>
2017-09-13 11:34:38 +0700

    update compile

--- compile-all.sh
+++ compile-all.sh
@@ -1,6 +1,6 @@
-rm -rf build/dc7-api-win-win32-ia32
-rm -rf build/dc7-api-win-win32-x64
-rm -rf build/dc7-api-darwin-x64
+rm -rf dc7-api-win-win32-ia32
+rm -rf dc7-api-win-win32-x64
+rm -rf dc7-api-darwin-x64
 #rm -rf dc7-api-linux-linux-x64
 
 ng build --base-href=./
@@ -19,8 +19,8 @@ scp src/package.json.txt dist/package.json
 #electron-packager ./dist dc7-api-win --platform=win32 --arch=ia32 --icon=./assets/moph.ico --asar --electron-version=1.6.10
 #electron-packager ./dist dc7-api-linux --platform=linux --arch=x64 --icon=./assets/moph.ico --asar --electron-version=1.6.10
 
-electron-packager ./dist ./build/dc7-api --platform=darwin --arch=x64  --asar --electron-version=1.6.10
-electron-packager ./dist ./build/dc7-api-win --platform=win32 --arch=ia32 --asar --electron-version=1.6.10
-electron-packager ./dist ./build/dc7-api-win --platform=win32 --arch=x64 --asar --electron-version=1.6.10
+electron-packager ./dist dc7-api --platform=darwin --arch=x64  --asar --electron-version=1.6.10
+electron-packager ./dist dc7-api-win --platform=win32 --arch=ia32 --asar --electron-version=1.6.10
+electron-packager ./dist dc7-api-win --platform=win32 --arch=x64 --asar --electron-version=1.6.10
 
-open ./build/dc7-api-darwin-x64/.
+open ./dc7-api-darwin-x64/.
